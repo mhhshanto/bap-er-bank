7d7fe8b64a2c80706189e325d211bcc3ff4163bf
shanto <hhassibul@gmail.com>
2022-06-01 12:24:15 +0600

    alldone

--- banking.js
+++ banking.js
@@ -1,11 +1,22 @@
-document.getElementById("deposit-button").addEventListener('click', function(){
- 
-  const amountA = parseFloat(document.getElementById("deposit-input").value);
-  
-  const neAmount = parseFloat(document.getElementById('deposit-total').innerText);
-  document.getElementById('deposit-total').innerText = neAmount + amountA;
+// deposit update
+document.getElementById('deposit-button').addEventListener('click', function(){
+  const inputAmount = parseFloat(document.getElementById('deposit-input').value);
 
+  const depositAmount =parseFloat(document.getElementById('deposit-total').innerText)
+  document.getElementById('deposit-total').innerText =inputAmount + depositAmount ;
+  // balance update 
+  const balanceAmount = parseFloat(document.getElementById('balance-total').innerText);
+  document.getElementById('balance-total').innerText = balanceAmount + inputAmount;
+  // cleaning
+  document.getElementById('deposit-input').value = '';
+})
+// withdraw update
+document.getElementById('withdraw-button').addEventListener('click', function(){
+  const withdrawInputAmount = parseFloat(document.getElementById('withdraw-input').value);
+  const withdrawAmount = parseFloat(document.getElementById('withdraw-total').innerText);
+  const balanceeAmount = parseFloat(document.getElementById('balance-total').innerText);
+  document.getElementById('withdraw-total').innerText = withdrawInputAmount + withdrawAmount;
+  document.getElementById('balance-total').innerText = balanceeAmount - withdrawInputAmount;
 
-  //cleaning 
-  document.getElementById('deposit-input').value = "";
+  document.getElementById('withdraw-input').value = "";
 })
