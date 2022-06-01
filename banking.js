39aac9459baef82c3a871ae4d6fa59d1bfc26bac
shanto <hhassibul@gmail.com>
2022-06-01 10:55:00 +0600

    3rd

--- banking.js
+++ banking.js
@@ -1,12 +1,9 @@
 document.getElementById("deposit-button").addEventListener('click', function(){
-  const amount = document.getElementById("deposit-input");
-  const amoun = amount.value;
-  const amountA = parseFloat(amoun);
-  document.getElementById('deposit-total').innerText = amount;
+  const amount = document.getElementById("deposit-input").value;
+  const amountA = parseFloat(amount);
   const newAmount = document.getElementById('deposit-total').innerText;
   const neAmount = parseFloat(newAmount);
   document.getElementById('deposit-total').innerText = neAmount + amountA;
-
 
 
   //cleaning 
